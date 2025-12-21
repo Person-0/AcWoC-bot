2027d276c83112132899803e304fdede928ee6fd
Person-0 <atulyajain28122007@gmail.com>
2025-12-21 16:40:10 +0530

    updated the ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,13 +2,15 @@ import { Message } from "discord.js";
 import { Command, CommandInfos } from "../commands.js";
 
 async function callback(info: CommandInfos, args: string[] = []) {
+    const init = Date.now();
     if(info instanceof Message) {
-        info.reply("pong");
+        const replymsg = await info.reply(":ping_pong: _ _");
+        await replymsg.edit(":ping_pong:  `[SERVER -> DISCORD]: " + (Date.now()-init) + "ms`");
     }
 }
 
 export default ({
     name: "ping",
-    description: "ping",
+    description: "Test bot's ping i.e latency",
     callback
 } satisfies Command);
