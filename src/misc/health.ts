7932f4317b806007ed463842df9ea0edec6397fc
Person-0 <atulyajain28122007@gmail.com>
2025-12-25 07:01:45 +0530

    make sure leaderboard doesnt go offline

--- src/misc/health.ts
+++ src/misc/health.ts
@@ -6,9 +6,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.send("hello world");
-})
+    try {
+        fetch(process.env.LEADERBOARD_FETCH_URL as string);
+    } catch (e) {}
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     log("web service runing on PORT", PORT);
-})
+});
